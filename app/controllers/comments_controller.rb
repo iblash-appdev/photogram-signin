79c9ac4344c00825c2933b2d74b6581ab4b7ae8e
Milton Lash <iblash@live.com>
2023-04-18 16:25:01 +0000

    Photos routes and views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,19 +7,25 @@ class CommentsController < ApplicationController
     comment = Comment.new
  
  
-    comment.author_id = params.fetch("input_author_id")
-    comment.photo_id = params.fetch("input_photo_id")
-    comment.body = params.fetch("input_body")
+    comment.author_id = session.fetch(:user_id) #arams.fetch("input_author_id")
+    comment.photo_id = params.fetch(:input_photo_id, nil)
+    comment.body = params.fetch(:input_body, nil)
  
  
     comment.save
- 
- 
-    redirect_to("/photos/#{comment.photo_id}")
+
+    respond_to do |format|
+      format.json do
+        render({ :json => comment.as_json })
+      end
+
+      format.html do
+        redirect_to("/photos/#{comment.photo_id}")
+      end
   end
  
+end
  
  
- 
- end
+end
  
